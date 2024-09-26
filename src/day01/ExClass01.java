42988d76e234d43c4d1f728d472e11d80d0bf58f
hoyoung <skghdud024@naver.com>
2024-09-26 14:17:09 +0900

    자바

---
+++ src/day01/ExClass01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class ExClass01 {
+
+	public static void main(String[] args) {
+		// main은 프로그램의 시작점
+		// 한줄 주석
+		/* 문단 주석 */
+//			syso + ctrl + space 자동완성
+//			sysout는 괄호의 내용을 출력하는 용도
+		System.out.println(100);
+		System.err.println("안녕하세요");
+		System.out.println("1.안녕");
+		System.out.println("2.안녕");
+		System.out.println("3.안녕");
+	}
+}
---
+++ src/day01/ExClass02.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class ExClass02 {
+
+	public static void main(String[] args) {
+		System.out.println("안\n녕\n하\n세\n요");
+		System.out.println("1\t2345678\t12345678\t1");
+		System.out.println(" \" \\ ");
+		System.out.println("D:\\핀테크 평일반 나호영");
+	}
+
+}
